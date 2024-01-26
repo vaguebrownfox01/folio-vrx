2ceb9b1cad338c3b6989415ae7079f24aa31b9dd
vaguebrownfox01 <jeevankylash@outlook.com>
2024-01-26 16:49:19 +0530

    anim

--- app/page.tsx
+++ app/page.tsx
@@ -25,18 +25,18 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-teal-300/50 to-zinc-300/0" />
 
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={128}
       />
 
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        jeevan
+      <h1 className="text-center pl-4 pr-4 pb-4 z-10 text-4xl text-transparent duration-1000 bg-gradient-to-tl from-white/25 via-teal-100 to-white/25 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        jeevan kylash
       </h1>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-teal-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
          {"vaguebrownfox"}
