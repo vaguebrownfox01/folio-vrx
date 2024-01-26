ad5810da39b572d8a8126ed17cd557d94b1debce
vaguebrownfox01 <jeevankylash@outlook.com>
2024-01-27 01:56:33 +0530

    clean

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 			</nav>
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-teal-300/50 to-zinc-300/0" />
 
-			<Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={128} />
+			<Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={512} />
 
 			<h1 className="text-center pl-4 pr-4 pb-4 z-10 text-4xl text-transparent duration-1000 bg-gradient-to-tl from-white/25 via-teal-100 to-white/25 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-6xl whitespace-nowrap bg-clip-text ">
 				jeevan kylash
