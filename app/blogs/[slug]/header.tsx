b317253556e6b10feb67c085f9ab23f4ae72ef03
jeevan kylash <jeevankylash@outlook.com>
2024-01-27 01:15:58 +0000

    blog page setup

--- app/blogs/[slug]/header.tsx
+++ app/blogs/[slug]/header.tsx
@@ -1,39 +1,45 @@
-'use client'
-import { ArrowLeft, Eye, Github, Twitter } from 'lucide-react'
-import Link from 'next/link'
-import React, { useEffect, useRef, useState } from 'react'
+'use client';
+import { ArrowLeft, Eye, Github, Twitter } from 'lucide-react';
+import Link from 'next/link';
+import React, { useEffect, useRef, useState } from 'react';
+import { page } from '../about';
 
 type Props = {
-	project: {
-		url?: string
-		title: string
-		description: string
-		repository?: string
-	}
+	post: {
+		url?: string;
+		title: string;
+		description: string;
+		repository?: string;
+	};
 
-	views: number
-}
-export const Header: React.FC<Props> = ({ project, views }) => {
-	const ref = useRef<HTMLElement>(null)
-	const [isIntersecting, setIntersecting] = useState(true)
+	views: number;
+};
+export const Header: React.FC<Props> = ({ post, views }) => {
+	const ref = useRef<HTMLElement>(null);
+	const [isIntersecting, setIntersecting] = useState(true);
 
-	const links: { label: string; href: string }[] = []
-
-	if (project.url) {
+	const links: { label: string; href: string }[] = [];
+	if (post.repository) {
 		links.push({
-			label: 'src',
-			href: project.url,
-		})
+			label: 'GitHub',
+			href: `https://github.com/${post.repository}`,
+		});
+	}
+	if (post.url) {
+		links.push({
+			label: 'Website',
+			href: post.url,
+		});
 	}
 	useEffect(() => {
-		if (!ref.current) return
+		if (!ref.current) return;
 		const observer = new IntersectionObserver(([entry]) =>
 			setIntersecting(entry.isIntersecting),
-		)
+		);
 
-		observer.observe(ref.current)
-		return () => observer.disconnect()
-	}, [])
+		observer.observe(ref.current);
+		return () => observer.disconnect();
+	}, []);
 
 	return (
 		<header
@@ -41,28 +47,28 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 			className="relative isolate overflow-hidden bg-gradient-to-tl from-black via-zinc-900 to-black"
 		>
 			<div
-				className={`fixed inset-x-0 top-0 z-50 backdrop-blur lg:backdrop-blur-none duration-200 border-b lg:bg-transparent ${
+				className={`fixed inset-x-0 top-0 z-50 border-b backdrop-blur duration-200 lg:bg-transparent lg:backdrop-blur-none ${
 					isIntersecting
-						? 'bg-zinc-900/0 border-transparent'
-						: 'bg-white/10  border-zinc-200 lg:border-transparent'
+						? 'border-transparent bg-zinc-900/0'
+						: 'border-zinc-200  bg-white/10 lg:border-transparent'
 				}`}
 			>
-				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
+				<div className="container mx-auto flex flex-row-reverse items-center justify-between p-6">
 					<div className="flex justify-between gap-8">
 						<span
 							title="View counter for this page"
-							className={`duration-200 hover:font-medium flex items-center gap-1 ${
+							className={`flex items-center gap-1 duration-200 hover:font-medium ${
 								isIntersecting
 									? ' text-zinc-400 hover:text-zinc-100'
 									: 'text-zinc-600 hover:text-zinc-900'
 							} `}
 						>
-							<Eye className="w-5 h-5" />{' '}
+							<Eye className="h-5 w-5" />{' '}
 							{Intl.NumberFormat('en-US', { notation: 'compact' }).format(views)}
 						</span>
 						<Link target="_blank" href="https://twitter.com/">
 							<Twitter
-								className={`w-6 h-6 duration-200 hover:font-medium ${
+								className={`h-6 w-6 duration-200 hover:font-medium ${
 									isIntersecting
 										? ' text-zinc-400 hover:text-zinc-100'
 										: 'text-zinc-600 hover:text-zinc-900'
@@ -71,7 +77,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 						</Link>
 						<Link target="_blank" href="https://github.com/">
 							<Github
-								className={`w-6 h-6 duration-200 hover:font-medium ${
+								className={`h-6 w-6 duration-200 hover:font-medium ${
 									isIntersecting
 										? ' text-zinc-400 hover:text-zinc-100'
 										: 'text-zinc-600 hover:text-zinc-900'
@@ -81,30 +87,28 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 					</div>
 
 					<Link
-						href="/blogs"
+						href={`/${page.name}`}
 						className={`duration-200 hover:font-medium ${
 							isIntersecting
 								? ' text-zinc-400 hover:text-zinc-100'
 								: 'text-zinc-600 hover:text-zinc-900'
 						} `}
 					>
-						<ArrowLeft className="w-6 h-6 " />
+						<ArrowLeft className="h-6 w-6 " />
 					</Link>
 				</div>
 			</div>
-			<div className="container mx-auto relative isolate overflow-hidden  py-24 sm:py-32">
-				<div className="mx-auto max-w-7xl px-6 lg:px-8 text-center flex flex-col items-center">
+			<div className="container relative isolate mx-auto overflow-hidden  py-24 sm:py-32">
+				<div className="mx-auto flex max-w-7xl flex-col items-center px-6 text-center lg:px-8">
 					<div className="mx-auto max-w-2xl lg:mx-0">
-						<h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl font-display">
-							{project.title}
+						<h1 className="font-display text-4xl font-bold tracking-tight text-white sm:text-6xl">
+							{post.title}
 						</h1>
-						<p className="mt-6 text-lg leading-8 text-zinc-300">
-							{project.description}
-						</p>
+						<p className="mt-6 text-lg leading-8 text-zinc-300">{post.description}</p>
 					</div>
 
 					<div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-						<div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+						<div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
 							{links.map((link) => (
 								<Link target="_blank" key={link.label} href={link.href}>
 									{link.label} <span aria-hidden="true">&rarr;</span>
@@ -115,5 +119,5 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 				</div>
 			</div>
 		</header>
-	)
-}
+	);
+};
