8f027f0e30bd5da10d7d788f5df8fe706ed27e72
vaguebrownfox <foxjournal.nsfw@gmail.com>
2024-02-28 23:42:22 +0530

    cleanup

--- app/_components/tag.tsx
+++ app/_components/tag.tsx
@@ -68,58 +68,7 @@ const project_tags = {
 const txhue = {
 	...art_tags,
 	...project_tags,
-
-	violet: 'text-violet-400',
-	pink: 'text-pink-400',
-	blue: 'text-blue-400',
-	green: 'text-lime-400',
-	yellow: 'text-yellow-400',
-	orange: 'text-orange-400',
-	red: 'text-red-400',
 };
-
-const bhue = {
-	'env.vua': 'border-violet-400',
-	jxp: 'border-pink-300',
-	fau: 'border-red-400',
-	concepts: 'border-orange-300',
-	sketchbook: 'border-amber-800',
-	procreate: 'border-pink-500',
-	illustration: 'border-yellow-400',
-	practice: 'border-gray-400',
-	design: 'border-purple-400',
-	'spire-lab': 'border-orange-400',
-	'data-collection': 'border-yellow-500',
-	'react-js': 'border-cyan-400',
-	'bio-medical': 'border-red-200',
-	'product-design': 'border-pink-300',
-	'machine-learning': 'border-yellow-300',
-	'signal-processing': 'border-violet-400',
-	'speech-processing': 'border-orange-500',
-	'ml-ops': 'border-yellow-400',
-	fusion360: 'border-orange-200',
-	'web-app': 'border-green-400',
-	matlab: 'border-orange-400',
-	firebase: 'border-yellow-600',
-	webpage: 'border-green-200',
-	'UI/UX': 'border-cyan-600',
-	'raspberry-pi': 'border-pink-500',
-	event: 'border-yellow-400',
-	'embedded-sys': 'border-violet-400',
-	'app-dev': 'border-blue-500',
-	android: 'border-green-500',
-	python: 'border-blue-400',
-	c: 'border-pink-500',
-	gratitude: 'border-white',
-	paper: 'border-white',
-	violet: 'border-violet-400',
-	pink: 'border-pink-400',
-	blue: 'border-blue-400',
-	green: 'border-lime-400',
-	yellow: 'border-yellow-400',
-	orange: 'border-orange-400',
-	red: 'border-red-400'
-  }
 
 const Tag: React.FC<Tag> = ({ tag }) => {
 	// const bhue = text2border(txhue);
