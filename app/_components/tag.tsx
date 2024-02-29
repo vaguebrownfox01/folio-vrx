8653319ff0c1a810b1c6ca94081f463b0618524e
vaguebrownfox <foxjournal.nsfw@gmail.com>
2024-02-29 08:33:04 +0530

    bhue

--- app/_components/tag.tsx
+++ app/_components/tag.tsx
@@ -1,3 +1,5 @@
+'use_client';
+
 type Tag = {
 	tag: string;
 };
@@ -20,71 +22,30 @@ const text2border = (textTags: object) => {
 };
 
 const art_tags = {
-	'env.vua': 'text-violet-400',
-	jxp: 'text-pink-300',
-	fau: 'text-red-400',
-
-	concepts: 'text-orange-300',
-	sketchbook: 'text-amber-800',
-	procreate: 'text-pink-500',
-
-	illustration: 'text-yellow-400',
-	practice: 'text-gray-400',
-	design: 'text-purple-400',
-	paper: 'text-white',
-};
-
-const project_tags = {
-	'spire-lab': 'text-orange-400',
-
-	'data-collection': 'text-yellow-500',
-	'react-js': 'text-cyan-400',
-	'bio-medical': 'text-red-200',
-	'product-design': 'text-pink-300',
-	'machine-learning': 'text-yellow-300',
-	klatt: 'text-green-400',
-
-	'signal-processing': 'text-violet-400',
-	'speech-processing': 'text-orange-500',
-	'ml-ops': 'text-yellow-400',
-	fusion360: 'text-orange-200',
-	'web-app': 'text-green-400',
-	matlab: 'text-orange-400',
-	firebase: 'text-yellow-600',
-	webpage: 'text-green-200',
-	'UI/UX': 'text-cyan-600',
-	'raspberry-pi': 'text-pink-500',
-	event: 'text-yellow-400',
-	'embedded-sys': 'text-violet-400',
-
-	'app-dev': 'text-blue-500',
-	android: 'text-green-500',
-
-	python: 'text-blue-400',
-	c: 'text-pink-500',
-
-	gratitude: 'text-white',
-	'user-research': 'text-white',
-};
-
-const bhue = {
 	'env.vua': 'border-violet-400',
 	jxp: 'border-pink-300',
 	fau: 'border-red-400',
+
 	concepts: 'border-orange-300',
 	sketchbook: 'border-amber-800',
 	procreate: 'border-pink-500',
+
 	illustration: 'border-yellow-400',
 	practice: 'border-gray-400',
 	design: 'border-purple-400',
 	paper: 'border-white',
+};
+
+const project_tags = {
 	'spire-lab': 'border-orange-400',
+
 	'data-collection': 'border-yellow-500',
 	'react-js': 'border-cyan-400',
 	'bio-medical': 'border-red-200',
 	'product-design': 'border-pink-300',
 	'machine-learning': 'border-yellow-300',
 	klatt: 'border-green-400',
+
 	'signal-processing': 'border-violet-400',
 	'speech-processing': 'border-orange-500',
 	'ml-ops': 'border-yellow-400',
@@ -97,31 +58,38 @@ const bhue = {
 	'raspberry-pi': 'border-pink-500',
 	event: 'border-yellow-400',
 	'embedded-sys': 'border-violet-400',
+
 	'app-dev': 'border-blue-500',
 	android: 'border-green-500',
+
 	python: 'border-blue-400',
 	c: 'border-pink-500',
+
 	gratitude: 'border-white',
 	'user-research': 'border-white',
 };
 
-const txhue = {
+// const txhue = {
+// 	...art_tags,
+// 	...project_tags,
+// };
+
+const bhue = {
 	...art_tags,
 	...project_tags,
 };
 
+
 const Tag: React.FC<Tag> = ({ tag }) => {
-	const bhue = text2border(txhue);
-	console.log(bhue);
 
 	// console.log(tag, undefined ? "true" : "false");
 	// console.log(txhue[tag as keyof typeof txhue].replace('text', 'border'));
 	return (
 		<div
-			className={`mb-2 mr-4 rounded-s border-l-2 ${bhue[tag as keyof typeof txhue]}`}
+			className={`mb-2 mr-4 rounded-s border-l-2 ${bhue[tag as keyof typeof bhue]}`}
 		>
 			<p
-				className={`pl-2 text-center text-sm ${txhue[tag as keyof typeof txhue] ? txhue[tag as keyof typeof txhue] : 'text-white'}`}
+				className={`pl-2 text-center text-sm ${bhue[tag as keyof typeof bhue]?.replace("border", "text")}`}
 			>
 				{tag}
 			</p>
